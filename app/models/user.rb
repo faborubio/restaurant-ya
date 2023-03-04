bb93abd232a7c51513ab14cedbd8e388066367f1
Fabian Rubio <fabian.rubiocs@gmail.com>
2023-03-04 13:38:40 -0300

    proyecto oka

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Asociaciones
+  has_many :restaurants
   # Active Record
   has_one_attached :photo
 end
